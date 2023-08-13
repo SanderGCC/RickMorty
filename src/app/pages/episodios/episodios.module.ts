b072eb127a9bbbf628fab01360f3f887b306d535
Sander Castillo <sandergcc@gmail.com>
2023-08-12 22:33:17 -0500

    Creacion del maquetado del proyecto

---
+++ src/app/pages/episodios/episodios.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EpisodiosComponent } from './episodios.component';
+
+
+
+@NgModule({
+  declarations: [
+    EpisodiosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EpisodiosModule { }
---
+++ src/app/pages/lugares/lugares.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LugaresComponent } from './lugares.component';
+
+
+
+@NgModule({
+  declarations: [
+    LugaresComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LugaresModule { }
